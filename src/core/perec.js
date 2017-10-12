0e5e2d80007997fc50a6113fdc25b50bb5b92e15
Gabriele Barbieri <gabriele.barbieri83@gmail.com>
2017-10-12 23:58:51 +0200

    Implement a perec semantic generator

---
+++ src/core/perec.js
@@ -0,0 +1,35 @@
+var markov = require('./markovchain')
+var dylanMatrices = require('./dylan_matrices.json')
+var similarities = require('./word_similarities.json')
+var _ = require('lodash')
+
+var ORDER = dylanMatrices.length - 1
+
+function generateSentences (constraints, n) {
+  var mc = markov.getMarkovProcess(dylanMatrices, constraints)
+  return _.times(n, i => markov.generate(mc, ORDER))
+}
+
+function getConstraints (words, index, length) {
+  var cts = _.fill(Array(length), null)
+  cts[index] = words
+  cts.unshift(['<s>'])
+  cts.push(['</s>'])
+  return cts
+}
+
+function generateSemanticSentence (sense, length, n) {
+  var words = similarities[sense]
+  var indices = _.shuffle(_.range(length))
+  for (var i = 0; i < length; i++) {
+    var index = indices[i]
+    var cts = getConstraints(words, index, length)
+    try {
+      return generateSentences(cts, n)
+    } catch (err) {
+    }
+  }
+}
+
+console.log(generateSemanticSentence('music', 10, 3))
+
