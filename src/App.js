9c0eb415b1b3d4961561523fc40984c009657b08
Gabriele Barbieri <gabriele.barbieri83@gmail.com>
2017-10-27 17:35:24 +0200

    Add the logo again 😎 😎

--- src/App.js
+++ src/App.js
@@ -7,13 +7,46 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} alt="logo" />
-        </header>
-        <p className="App-intro">{generate("music", 10, 1)}</p>
+        <Logo />
+        <Sentence className="App-intro" sense="music" />
+        <Doc />
       </div>
     );
   }
+}
+
+function Logo(props) {
+  return (
+    <header className="App-header">
+      <img src={logo} alt="logo" />
+    </header>
+  );
+}
+
+class Sentence extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { sentence: "Hello World" };
+  }
+
+  componentDidMount() {
+    this.setState({
+      sentence: "generate(this.props.sense, 10, 1)"
+    });
+  }
+
+  render() {
+    return <p>{this.state.sentence}</p>;
+  }
+}
+
+function Doc() {
+  return (
+    <p>
+      Continue to read the doc{" "}
+      <a href="https://reactjs.org/docs/handling-events.html">here</a>
+    </p>
+  );
 }
 
 export default App;
