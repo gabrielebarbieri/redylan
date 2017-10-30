41d656be9f15f8d1d53a449617a88c45b44cfcff
Gabriele Barbieri <gabriele.barbieri83@gmail.com>
2017-10-30 23:38:38 +0100

    Revert "Add the logo again 😎 😎"
    This reverts commit 9c0eb415b1b3d4961561523fc40984c009657b08.

--- src/App.js
+++ src/App.js
@@ -7,46 +7,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Logo />
-        <Sentence className="App-intro" sense="music" />
-        <Doc />
+        <header className="App-header">
+          <img src={logo} alt="logo" />
+        </header>
+        <p className="App-intro">{generate("music", 10, 1)}</p>
       </div>
     );
   }
-}
-
-function Logo(props) {
-  return (
-    <header className="App-header">
-      <img src={logo} alt="logo" />
-    </header>
-  );
-}
-
-class Sentence extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { sentence: "Hello World" };
-  }
-
-  componentDidMount() {
-    this.setState({
-      sentence: "generate(this.props.sense, 10, 1)"
-    });
-  }
-
-  render() {
-    return <p>{this.state.sentence}</p>;
-  }
-}
-
-function Doc() {
-  return (
-    <p>
-      Continue to read the doc{" "}
-      <a href="https://reactjs.org/docs/handling-events.html">here</a>
-    </p>
-  );
 }
 
 export default App;
