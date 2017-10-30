6cdba2af57b0f947042216225bf522d5502f3bea
Gabriele Barbieri <gabriele.barbieri83@gmail.com>
2017-10-30 23:38:47 +0100

    Revert "migrate from vue to react"
    This reverts commit 1279fc4e62ec67f0cd502ade7996d83c4ddf2a93.

---
+++ src/components/Hello.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="hello">
+    <h1>{{sentence}}</h1>
+    <div id="example-2">
+      <button v-on:click="test">Generate</button>
+    </div>
+    <!-- <h2>Essential Links</h2>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+      <br>
+      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+    </ul> -->
+  </div>
+</template>
+
+<script>
+  var Worker = require('worker-loader!@/core/worker')
+  var worker = new Worker()
+  worker.post = message =>
+    new Promise((resolve, reject) => {
+      worker.onmessage = event => {
+        resolve(event.data)
+      }
+      worker.onerror = e => {
+        console.error(`Error: Line ${e.lineno} in ${e.filename}: ${e.message}`)
+        reject(e)
+      }
+      worker.postMessage(message)
+    })
+
+  export default {
+    name: 'hello',
+    data () {
+      return {
+        sentence: 'Hello World'
+      }
+    },
+    methods: {
+      test: function () {
+        var vm = this
+        vm.sentence = 'Computing ... '
+        worker.post('music').then(function (e) {
+          console.log('Worker said: ', e)
+          vm.sentence = e[0]
+        })
+      }
+    }
+  }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1,
+  h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+
+</style>
--- src/core/perec.js
+++ src/core/perec.js
@@ -1,37 +1,36 @@
-var markov = require("./markovchain");
-var dylanMatrices = require("./dylan_matrices.json");
-var similarities = require("./word_similarities.json");
-var _ = require("lodash");
+var markov = require('./markovchain')
+var dylanMatrices = require('./dylan_matrices.json')
+var similarities = require('./word_similarities.json')
+var _ = require('lodash')
 
-var ORDER = dylanMatrices.length - 1;
+var ORDER = dylanMatrices.length - 1
 
-function generateSentences(constraints, n) {
-  var mc = markov.getMarkovProcess(dylanMatrices, constraints);
-  return _.times(n, i =>
-    _.join(_.slice(markov.generate(mc, ORDER), 1, -1), " ")
-  );
+function generateSentences (constraints, n) {
+  var mc = markov.getMarkovProcess(dylanMatrices, constraints)
+  return _.times(n, i => _.join(_.slice(markov.generate(mc, ORDER), 1, -1), ' '))
 }
 
-function getConstraints(words, index, length) {
-  var cts = _.fill(Array(length), null);
-  cts[index] = words;
-  cts.unshift(["<s>"]);
-  cts.push(["</s>"]);
-  return cts;
+function getConstraints (words, index, length) {
+  var cts = _.fill(Array(length), null)
+  cts[index] = words
+  cts.unshift(['<s>'])
+  cts.push(['</s>'])
+  return cts
 }
 
-function generateSemanticSentence(sense, length, n) {
-  var words = similarities[sense];
+function generateSemanticSentence (sense, length, n) {
+  var words = similarities[sense]
 
-  var indices = _.shuffle(_.range(length));
+  var indices = _.shuffle(_.range(length))
   for (var i = 0; i < length; i++) {
-    var index = indices[i];
-    var cts = getConstraints(words, index, length);
+    var index = indices[i]
+    var cts = getConstraints(words, index, length)
     try {
-      return generateSentences(cts, n);
-    } catch (err) {}
+      return generateSentences(cts, n)
+    } catch (err) {
+    }
   }
-  return null;
+  return null
 }
 
-module.exports = generateSemanticSentence;
+module.exports = generateSemanticSentence
