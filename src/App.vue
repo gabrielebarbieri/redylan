c812e3d9002d7a32a53c963763d9d8576551c514
Gabriele Barbieri <gabriele.barbieri83@gmail.com>
2018-01-19 22:20:25 +0100

    Style a navbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
   <el-container id="app">
-    <el-header>Home</el-header>
+    <el-header>
+      <el-row type="flex">
+        <el-col :span="2"><img src="./assets/logo.png" height="64px;" width="64px;" align="center"></el-col>
+        <el-col>
+      <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+        <el-menu-item index="1">Song generator</el-menu-item>
+        <el-menu-item index="3">How it works</el-menu-item>
+      </el-menu>
+        </el-col>
+      </el-row>
+    </el-header>
     <el-main>
-      <!-- <el-row>
-        <el-col :span="24"><img src="./assets/logo.png"></el-col>
-      </el-row> -->
       <el-row>
         <el-col :span="24"><router-view></router-view></el-col>
       </el-row>
@@ -31,12 +38,15 @@ body, html {
   margin: auto;
 }
 .el-header {
-    background-color: #303133;
+    /*background-color: #303133;*/
     color: #F2F6FC;
     /*text-align: center;*/
-    line-height: 60px;
+    line-height: 64px;
   }
 
+.el-menu {
+  border-bottom: none;
+}
 /*// Adobe CC Color scheme
 
 4D6973
