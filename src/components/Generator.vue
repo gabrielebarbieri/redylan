2069a502d1818d8044915b2bdafd8e2cd8fcb59f
Gabriele Barbieri <gabriele.barbieri83@gmail.com>
2018-01-19 12:50:23 +0100

    Simple page to test deployment to github pages

--- src/components/Generator.vue
+++ src/components/Generator.vue
@@ -2,79 +2,79 @@
   <div>
     <h2>{{sentence}}</h2>
     <el-row >
-      <search v-model="sense"></search>
-      <el-button type="primary" :loading="isLoading" v-on:click="fit" style="margin-left: 10px;">Fit</el-button>
-      <el-button type="primary" v-bind:disabled="markovProcess===null" v-on:click="generate">Generate</el-button>
+      <!-- <search v-model="sense"></search> -->
+      <!-- <el-button type="primary" :loading="isLoading" v-on:click="fit" style="margin-left: 10px;">Fit</el-button> -->
+      <!-- <el-button type="primary" v-bind:disabled="markovProcess===null" v-on:click="generate">Generate</el-button> -->
     </el-row>
-    <graph :graph="markovProcessGraph"></graph>
+    <!-- <graph :graph="markovProcessGraph"></graph> -->
   </div>
 </template>
 
 <script>
-  import Graph from '@/components/Graph'
-  import Search from '@/components/Search'
-  var perec = require('@/core/perec')
-  var Worker = require('worker-loader!@/core/worker')
-  var worker = new Worker()
-  worker.post = message =>
-    new Promise((resolve, reject) => {
-      worker.onmessage = event => {
-        resolve(event.data)
-      }
-      worker.onerror = e => {
-        console.error(`Error: Line ${e.lineno} in ${e.filename}: ${e.message}`)
-        reject(e)
-      }
-      worker.postMessage(message)
-    })
+  // import Graph from '@/components/Graph'
+  // import Search from '@/components/Search'
+  // var perec = require('@/core/perec')
+  // var Worker = require('worker-loader!@/core/worker')
+  // var worker = new Worker()
+  // worker.post = message =>
+  //   new Promise((resolve, reject) => {
+  //     worker.onmessage = event => {
+  //       resolve(event.data)
+  //     }
+  //     worker.onerror = e => {
+  //       console.error(`Error: Line ${e.lineno} in ${e.filename}: ${e.message}`)
+  //       reject(e)
+  //     }
+  //     worker.postMessage(message)
+  //   })
 
   export default {
     name: 'generator',
     data () {
       return {
-        sense: '',
-        sentence: 'Click on the fit button to train a model',
-        isLoading: false,
-        markovProcess: null,
-        sequence: null
+        // sense: '',
+        sentence: 'Click on the fit button to train a model'
+        // isLoading: false,
+        // markovProcess: null,
+        // sequence: null
       }
-    },
-    computed: {
-      markovProcessGraph: function () {
-        if (this.markovProcess !== null) {
-          var g = perec.convertToGraph(this.markovProcess)
-          perec.colorSentence(g, this.sequence)
-          return g
-        }
-      }
-    },
-    methods: {
-      fit: function () {
-        var vm = this
-        vm.markovProcess = null
-        vm.sequence = null
-        vm.sentence = 'Computing ... '
-        vm.isLoading = true
-        console.log(vm.sense)
-        worker.post(vm.sense).then(function (markovProcess) {
-          // vm.sequence = perec.generate(markovProcess)
-          // vm.sentence = perec.represent(vm.sequence)
-          vm.markovProcess = markovProcess
-          vm.sentence = 'Click on generate to get a sentence'
-          vm.isLoading = false
-        })
-      },
-      generate: function () {
-        var vm = this
-        if (vm.markovProcess === null) return
-        vm.sequence = perec.generate(vm.markovProcess)
-        vm.sentence = perec.represent(vm.sequence)
-      }
-    },
-    components: {
-      'graph': Graph,
-      'search': Search
     }
+    // computed: {
+    //   markovProcessGraph: function () {
+    //     if (this.markovProcess !== null) {
+    //       var g = perec.convertToGraph(this.markovProcess)
+    //       perec.colorSentence(g, this.sequence)
+    //       return g
+    //     }
+    //   }
+    // },
+    // methods: {
+    //   fit: function () {
+    //     var vm = this
+    //     vm.markovProcess = null
+    //     vm.sequence = null
+    //     vm.sentence = 'Computing ... '
+    //     vm.isLoading = true
+    //     console.log(vm.sense)
+    //     worker.post(vm.sense).then(function (markovProcess) {
+    //       // vm.sequence = perec.generate(markovProcess)
+    //       // vm.sentence = perec.represent(vm.sequence)
+    //       vm.markovProcess = markovProcess
+    //       vm.sentence = 'Click on generate to get a sentence'
+    //       vm.isLoading = false
+    //     })
+    //   },
+    //   generate: function () {
+    //     var vm = this
+    //     if (vm.markovProcess === null) return
+    //     vm.sequence = perec.generate(vm.markovProcess)
+    //     vm.sentence = perec.represent(vm.sequence)
+    //   }
+    // },
+    // components: {
+    //   'graph': Graph,
+    //   'search': Search
+    // }
   }
 </script>
 
