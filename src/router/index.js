ac6b7fd7ddc0f0a80dfca0d2d250d8b16406694b
Gabriele Barbieri <gabriele.barbieri83@gmail.com>
2018-01-22 21:33:45 +0100

    Add (empty) how it works page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Generator from '@/components/Generator'
+import HowItWorks from '@/components/HowItWorks'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Generator',
       component: Generator
+    },
+    {
+      path: '/how-it-works/',
+      name: 'HowItWorks',
+      component: HowItWorks
     }
   ]
 })
