fdf87cbac1dfa14240716cf9822a7c16d410697e
Gabriele Barbieri <gabriele.barbieri83@gmail.com>
2018-02-20 13:37:01 +0100

    Generate song inside a card

--- src/components/SongGenerator.vue
+++ src/components/SongGenerator.vue
@@ -1,22 +1,32 @@
 <template>
 	<div>
-		<h1>Generate a song in the style of Bob Dylan</h1>
-    Choose the structure of your song: <el-select v-model="scheme" placeholder="Song structure">
-      <el-option
-        v-for="rhyme in rhymes"
-        :key="rhyme.value"
-        :label="rhyme.label + ' (' + rhyme.value + ')'"
-        :value="rhyme.value">
-      </el-option>
-    </el-select>
     <el-row>
-      <span v-if="senses.length">Choose up to {{senses.length}} themes for your song:</span>
-      <search v-for="s in senses" :key="s.rhyme" v-model="s.sense"></search>
+      <el-col :span="12">
+        <h1>Generate a song in the style of Bob Dylan</h1>
+        Choose the structure of your song: <el-select v-model="scheme" placeholder="Song structure">
+          <el-option
+            v-for="rhyme in rhymes"
+            :key="rhyme.value"
+            :label="rhyme.label + ' (' + rhyme.value + ')'"
+            :value="rhyme.value">
+          </el-option>
+        </el-select>
+        <el-row>
+          <span v-if="senses.length">Choose up to {{senses.length}} themes for your song:</span>
+          <search v-for="s in senses" :key="s.rhyme" v-model="s.sense"></search>
+        </el-row>
+        <el-button type="primary" v-on:click="generateSong" :loading="isGenerating">Generate Song</el-button>
+      </el-col>
+      <el-col :span="8">
+        <el-card v-if="displaySong" :body-style="{ padding: '0px' }">
+          <img v-bind:src="songImage" class="image">
+          <div style="padding: 14px;">
+            <h3>{{title}}</h3>
+            <p><div v-for="verse in verses">{{verse}}<br></div></p>
+          </div>
+        </el-card>
+      </el-col>
     </el-row>
-		<el-button type="primary" v-on:click="generateSong" :loading="isGenerating">Generate Song</el-button>
-    <p>
-		  <div v-for="verse in verses">{{verse}}<br></div>
-    </p>
 	</div>
 </template>
 
@@ -31,8 +41,11 @@
       return {
         verses: [],
         isGenerating: false,
+        displaySong: false,
+        songImage: '',
         rhymes: rhymeSchemes,
         scheme: '',
+        title: '',
         senses: []
       }
     },
@@ -44,11 +57,26 @@
       }
     },
     methods: {
+      getSongImageUrl: function (senses) {
+        if (senses.length > 0) {
+          return 'https://source.unsplash.com/320x180/?' + senses[0]
+        }
+        return 'https://source.unsplash.com/320x180/?music'
+      },
       generateSong: function () {
         var vm = this
         vm.isGenerating = true
+        vm.displaySong = true
         vm.clean()
         var senses = _.filter(_.map(vm.senses, s => s.sense), s => s !== '')
+        vm.songImage = vm.getSongImageUrl(senses)
+
+        if (senses.length === 1) {
+          vm.title = _.capitalize(senses[0])
+        } else if (senses.length > 1) {
+          vm.title = _.capitalize(_.join(_.initial(senses), ', ') + ' and ' + _.last(senses))
+        }
+
         songWorker.generate(senses, vm.scheme, vm.addVerse, vm.songGenerated)
       },
       addVerse: function (verse) {
@@ -66,3 +94,11 @@
     }
 }
 </script>
+
+<style>
+.image {
+    width: 100%;
+    display: block;
+  }
+</style>
+
